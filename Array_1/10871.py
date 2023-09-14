ea7b852a8f96fa51cd84f010b6b36b2c99e937bf
Ju0011 <yhs07240@naver.com>
2023-09-14 21:35:41 +0900

    No_10871 answer

--- Array_1/10871.py
+++ Array_1/10871.py
@@ -1,3 +1,11 @@
 N, X = map(int, input().split())
 
 n_list = list(map(int, input().split()))
+answer_list = []
+
+for i in range(len(n_list)):
+    if X > n_list[i]:
+        answer_list.append(n_list[i])
+
+for i in range(len(answer_list)):
+    print(answer_list[i] , end=" ")
