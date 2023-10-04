8906c0d3a9eb4bd830784e7b1cfe6121a7fc46b0
Ju0011 <yhs07240@naver.com>
2023-10-04 11:18:48 +0900

    No_10809 answer

---
+++ String/10809.py
@@ -0,0 +1,12 @@
+S = list(input())
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+output =[-1]*len(alpha)
+
+for i in range(len(S)):
+    for j in range(len(alpha)):
+        if S[i] == alpha[j]:
+            if output[j] == -1:
+                output[j] = i
+\
+for i in range(len(output)):
+    print(output[i], end=' ')
